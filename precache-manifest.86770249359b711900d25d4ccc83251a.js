9152fd9db9f18a5be31866a482f6e5d59f793843
Quynh Pham <quynh.p@shopstack.asia>
2019-03-01 11:42:09 +0700

    Updates

---
+++ precache-manifest.86770249359b711900d25d4ccc83251a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cde22863ffca018955eb",
+    "url": "/react-gh-pages/static/js/runtime~main.cde22863.js"
+  },
+  {
+    "revision": "87884896929c69da4bc2",
+    "url": "/react-gh-pages/static/js/main.87884896.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/react-gh-pages/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "87884896929c69da4bc2",
+    "url": "/react-gh-pages/static/css/main.ea10e58a.chunk.css"
+  },
+  {
+    "revision": "dad7b2c5cb66a234069399888116faab",
+    "url": "/react-gh-pages/index.html"
+  }
+];
